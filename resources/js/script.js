34edbc9dff2d79dedb0ee3350777b95d67a83d11
NickSklA <nikos.skl98@gmail.com>
2019-07-01 19:52:49 +0300

    Add rating system function

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,3 +6,21 @@ window.onscroll = () => {
     else 
         nav.className = 'scroll';
 };
+
+// rating system
+function onStarClick(span) {
+
+    // get all stars
+    const stars = document.getElementsByClassName('span-star');
+
+    // remove checked class
+    for (const star of stars) {
+        star.classList.remove('checked');
+    }
+
+    // get clicked star
+    const c = span.childNodes;
+
+    // add new checked star
+    span.classList.add('checked');
+}

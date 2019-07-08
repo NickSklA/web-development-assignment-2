252d142d6302cec6d3d5abdb9355441a53324575
nikos.skl <nikos.skl98@gmail.com>
2019-07-08 12:38:50 +0300

    Add database with dummy data

--- dsflixdb.sql
+++ dsflixdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2019 at 12:20 PM
+-- Generation Time: Jul 07, 2019 at 01:14 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
